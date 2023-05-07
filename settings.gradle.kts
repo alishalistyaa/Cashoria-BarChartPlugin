14772568c7c2d9f7eb79b6e1d63bc520a9a5dd94
Alisha Listya <alisha.listya@gmail.com>
2023-05-08 01:40:30 +0700

    add: init

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Cashoria-BarChartPlugin"
+
---
+++ src/main/java/barchart/barplugin/ChartPlugin.java
@@ -0,0 +1,17 @@
+package barchart.barplugin;
+
+import chart.plugin.ChartPluginInterface;
+import controller.MainController;
+import model.FixedBill;
+import org.knowm.xchart.XChartPanel;
+
+import javax.swing.*;
+import java.util.List;
+
+public class ChartPlugin implements ChartPluginInterface {
+    @Override
+    public JPanel loadPanel(MainController mainController) {
+        List<FixedBill> temp = mainController.getFixedBillDataIO().getAll();
+        return new XChartPanel<>(new PieChartPlugin(temp).getChart());
+    }
+}
