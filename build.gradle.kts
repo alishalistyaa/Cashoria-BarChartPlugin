de1bafe03a504613bc9b3f6d036bf54ad28d95e6
Alisha Listya <alisha.listya@gmail.com>
2023-05-08 01:47:01 +0700

    add: init

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.knowm.xchart:xchart:3.2.2")
+    implementation("org.knowm.xchart:xchart-demo:3.5.2")
     implementation(files("/Users/alishalistya/VSCode/untitled/build/libs/untitled-1.0-SNAPSHOT.jar"))
     implementation(files("/Users/alishalistya/VSCode/Tubes-2-OOP-O08/build/libs/project.jar"))
 }
